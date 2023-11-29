9c338e67ee928b333ba8a61a6a74abd5c5284bd8
JomanahAljehani <jomannah1423@gmail.com>
2023-11-29 18:37:50 +0300

    Initial commit

---
+++ src/sensordataprocessor/SensorDataProcessor1.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sensordataprocessor;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+/**
+ *
+ * @author alwle
+ */
+public class SensorDataProcessor1 {
+
+    // Sensor data and limits.
+    public double[][][] data;
+    public double[][] limit;
+
+    // Constructor
+    public SensorDataProcessor1(double[][][] data, double[][] limit) {
+        this.data = data;
+        this.limit = limit;
+    }
+
+    // Calculates average of sensor data
+    private double average(double[] array) {
+        double sum = 0;
+        for (double value : array) {
+            sum += value;
+        }
+        return sum / array.length;
+    }
+
+    // Calculate data
+    public void calculate(double d) {
+        int i, j, k;
+        double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
+        BufferedWriter out = null;
+
+        try {
+            out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
+            for (i = 0; i < data.length; i++) {
+                for (j = 0; j < data[0].length; j++) {
+                    for (k = 0; k < data[0][0].length; k++) {
+                        data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
+                        if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50) {
+                            break;
+                        } else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k]) {
+                            break;
+                        } else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+                                && average(data[i][j]) < data2[i][j][k] && (i + 1) * (j + 1) > 0) {
+                            data2[i][j][k] *= 2;
+                        } else {
+                            continue;
+                        }
+                    }
+                }
+            }
+            for (i = 0; i < data2.length; i++) {
+                for (j = 0; j < data2[0].length; j++) {
+                    for (k = 0; k < data2[0][0].length; k++) {
+                        out.write(data2[i][j][k] + "\t");
+                    }
+                    out.newLine();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    System.out.println("Error closing the file: " + e.getMessage());
+                }
+            }
+        }
+    }
+}
+
